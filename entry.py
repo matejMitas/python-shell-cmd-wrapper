1f136f97bbd14192ebbd2604285ffe740566249d
Matej Mitas <contact@matejmitas.com>
2019-06-18 10:29:31 +0200

    Basic structure for library, parser has prototype class

--- entry.py
+++ entry.py
@@ -1,7 +1,10 @@
 from lib.library import Library
 
-compress_lib = Library('kakadu', library='kdu_compress')
-compress_lib.test()
+compress_lib = Library('kakadu', 'kdu_compress')
+compress_lib.set_io('file1.ppm', 'file2.jp2')
+compress_lib.set_fixed()
+compress_lib.construct()
 
 convert_lib = Library('convert')
-convert_lib.test()
+convert_lib.set_io('in_file.jpg', 'out_file.jpg')
+convert_lib.construct()
--- lib/library.py
+++ lib/library.py
@@ -1,6 +1,88 @@
-class Library:
-	def __init__(self, blueprint_name, **kwargs):
-		self.blueprint_name = blueprint_name
+"""
+Business logic of the main module.
+"""
 
-	def test(self):
+"""
+Generic library imports
+"""
+
+"""
+Class imports
+"""
+from lib._blueprint_parser import Parser
+"""
+Functional imports
+"""
+
+class Library:
+	def __init__(self, blueprint_name, lib_name=None):
+		"""
+		Each blueprint can contain multiple libraries so further
+		distinguishing needs to take place in form of lib_name providing
+		actual name
+		"""
+		self.blueprint_name = blueprint_name
+		self.lib_name = lib_name
+		"""
+		Fixed flags can only be set once 
+		"""
+		self.fixed_set = False
+		"""
+		Internal structure for storing flags
+		"""
+		self.structure = {
+			'io'		: {
+				'input'			: None,
+				'output'		: None
+			},
+			'fixed'		: {
+				'blueprint'		: {},
+				'transformed'	: []
+			},
+			'variable'	: {
+				'blueprint'		: {},
+				'curr'			: 0
+			}
+		}
+
+	"""
+	PUBLIC methods
+	"""
+	def reset(self):
+		"""
+		Resets
+		"""
+		self.fixed_set = False
+
+	def construct(self):
+		output_buffer = []
+
+		output_buffer += self._generate_io()
+
+		print(output_buffer)
+		
+		return {
+			'index': 0,
+			'items': []
+		}
+
+	def set_io(self, input_file=None, output_file=None):
+		if input_file:
+			self.structure['io']['input'] = input_file
+		if output_file:
+			self.structure['io']['output'] = output_file
+
+	def set_fixed(self, **kwargs):
+		pass
+
+	def set_variable(self, **kwargs):
+		pass
+
+	"""
+	PRIVATE methods
+	"""
+	def _generate_io(self):
+		return ['-i', self.structure['io']['input'], '-o', self.structure['io']['output']]
+
+	def _get_blueprint(self):
 		print(self.blueprint_name)
